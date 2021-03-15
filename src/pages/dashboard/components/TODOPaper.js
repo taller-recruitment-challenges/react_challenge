a30f61012b14c7af577757bee6bd821714541049
Franco Agresta Gonzalez <franquitt@gmail.com>
2021-03-15 15:21:30 -0300

    change row

--- src/pages/dashboard/components/TODOPaper.js
+++ src/pages/dashboard/components/TODOPaper.js
@@ -3,7 +3,6 @@ import {DataGrid} from '@material-ui/data-grid';
 
 const columns = [
 	{field: 'id', headerName: 'ID', width: 70},
-	{field: 'UserName', headerName: 'Username', width: 130},
 	{field: 'title', headerName: 'Title', width: 130},
 	{field: 'completed', headerName: 'Completed', width: 130},
 ];
